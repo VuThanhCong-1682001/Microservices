1c6fb611ebcd4ba363cff145b7519b433aa636d1
metech_thanhcong <thanhcong16082001@gmail.com>
2023-03-19 00:43:48 +0700

    Seed data inventory

---
+++ src/BuildingBlocks/Shared/Dtos/Inventory/PurchaseProductDto.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Enums.Inventory;
+
+namespace Shared.DTOs.Inventory;
+
+public record PurchaseProductDto
+{
+    //public EDocumentType DocumentType => EDocumentType.Purchase;
+
+    //private string _itemNo { get; set; }
+
+    //public string GetItemNo() => _itemNo;
+
+    //public void SetItemNo(string itemNo)
+    //{
+    //    _itemNo = itemNo;
+    //}
+
+    public string ItemNo { get; set; }
+    public string DocumentNo { get; set; } // Sample: PO-2023-02-XXXXX
+    public string ExternalDocumentNo { get; set; }
+    public int Quantity { get; set; }
+}
--- src/Services/Inventory.Product.API/Program.cs
+++ src/Services/Inventory.Product.API/Program.cs
@@ -1,17 +1,24 @@
 using Common.Logging;
+using Inventory.Product.API.Extensions;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog(Serilogger.Configure);
 
+Log.Information($"Start {builder.Environment.ApplicationName} up");
 Log.Information("Starting Inventory Product API up");
+
 try
 {
-
     // Add services to the container.
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
+    builder.Services.AddConfigurationSettings(builder.Configuration);
+    builder.Services.AddInfrastructureServices();
+    builder.Services.ConfigureMongoDbClient();
 
     var app = builder.Build();
 
@@ -26,13 +33,16 @@ try
 
     app.UseAuthorization();
 
-    app.MapControllers();
+    app.MapDefaultControllerRoute();
 
-    app.Run();
+    app.MigrateDatabase().Run();
 }
 catch (Exception ex)
 {
-    Log.Fatal(ex, "Unhandled exception");
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal)) throw;
+
+    Log.Fatal(ex, $"Unhandled exception: {ex.Message}");
 }
 finally
 {
