8fbdcfef0c903ce66a8bba2dbddf1d0a7fda5b86
congvt <congvt@metechvn.com>
2023-06-03 13:14:11 +0700

    authorized product api and generate token jwt

--- src/Services/Product.API/Extensions/ApplicationExtensions.cs
+++ src/Services/Product.API/Extensions/ApplicationExtensions.cs
@@ -1,14 +1,20 @@
-﻿namespace Product.API.Extensions
+﻿using Infrastructure.Middlewares;
+
+namespace Product.API.Extensions
 {
     public static class ApplicationExtensions
     {
         public static void UseInfrastructure(this IApplicationBuilder app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API"));
+
+            app.UseMiddleware<ErrorWrappingMiddleware>();
+
+            app.UseAuthentication();
 
             app.UseRouting();
-            app.UseHttpsRedirection(); //for production only
+            //app.UseHttpsRedirection(); //for production only
 
             app.UseAuthorization();
 
