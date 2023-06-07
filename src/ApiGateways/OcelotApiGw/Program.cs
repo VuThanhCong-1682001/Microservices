9300d0ed5730d8b7118bb15e5020f854f69e26d6
congvt <congvt@metechvn.com>
2023-06-08 06:51:24 +0700

    Containerize API Gateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -30,9 +30,9 @@ try
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
-            $"{builder.Environment.ApplicationName} v1"));
+        //app.UseSwagger();
+        //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json",
+        //    $"{builder.Environment.ApplicationName} v1"));
     }
 
     app.UseCors("CorsPolicy");
