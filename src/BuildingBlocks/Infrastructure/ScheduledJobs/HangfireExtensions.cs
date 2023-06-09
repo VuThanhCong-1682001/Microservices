cf017310ef409d4c6becd12838489d22fc1d5c1f
congvt <congvt@metechvn.com>
2023-06-09 07:28:28 +0700

    Configure Hangfire 1

---
+++ src/BuildingBlocks/Infrastructure/ScheduledJobs/HangfireExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Hangfire;
+using Infrastructure.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.ScheduledJobs;
+
+public static class HangfireExtensions
+{
+    public static IServiceCollection AddHangfireService(this IServiceCollection services)
+    {
+        var settings = services.GetOptions<HangFireSettings>("HangFireSettings");
+        if (settings == null || settings.Storage == null ||
+            string.IsNullOrEmpty(settings.Storage.ConnectionString))
+            throw new Exception("HangFireSettings is not configured properly!");
+
+        services.AddHangfireServer(serverOptions => { serverOptions.ServerName = settings.ServerName; });
+        
+        return services;
+    }
+}
---
+++ src/Services/Hangfire.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Hangfire.API.Extensions;
+
+public static class ServiceExtensions
+{
+    internal static IServiceCollection AddConfigurationSettings(this IServiceCollection services,
+       IConfiguration configuration)
+    {
+        var hangFireSettings = configuration.GetSection(nameof(HangFireSettings))
+            .Get<HangFireSettings>();
+        services.AddSingleton(hangFireSettings);
+
+        return services;
+    }
+}
