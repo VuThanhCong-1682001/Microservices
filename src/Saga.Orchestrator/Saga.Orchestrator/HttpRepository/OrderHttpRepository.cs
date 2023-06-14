f7ce3c5285cfd5f7de3220744f0f45c26403a436
congvt <congvt@metechvn.com>
2023-06-14 07:06:45 +0700

    Saga pattern 2

---
+++ src/Saga.Orchestrator/Saga.Orchestrator/HttpRepository/OrderHttpRepository.cs
@@ -0,0 +1,43 @@
+﻿using Saga.Orchestrator.HttpRepository.Interfaces;
+using Shared.DTOs.Order;
+using Shared.SeedWork;
+using Infrastructure.Extensions;
+
+namespace Saga.Orchestrator.HttpRepository;
+
+public class OrderHttpRepository : IOrderHttpRepository
+{
+    private readonly HttpClient _httpClient;
+
+    public OrderHttpRepository(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<long> CreateOrder(CreateOrderDto order)
+    {
+        var response = await _httpClient.PostAsJsonAsync("orders", order);
+        if (!response.EnsureSuccessStatusCode().IsSuccessStatusCode) return -1;
+
+        var orderId = await response.ReadContentAs<ApiSuccessResult<long>>();
+        return orderId.Data;
+    }
+
+    public async Task<OrderDto> GetOrder(long id)
+    {
+        var order = await _httpClient.GetFromJsonAsync<ApiSuccessResult<OrderDto>>($"orders/{id}");
+        return order.Data;
+    }
+
+    public async Task<bool> DeleteOrder(long id)
+    {
+        var response = await _httpClient.DeleteAsync($"orders/{id}");
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> DeleteOrderByDocumentNo(string documentNo)
+    {
+        var repsonse = await _httpClient.DeleteAsync($"document-no/{documentNo}");
+        return repsonse.IsSuccessStatusCode;
+    }
+}
--- src/Services/Ordering/Ordering.Application/Common/Interfaces/IOrderRepository.cs
+++ src/Services/Ordering/Ordering.Application/Common/Interfaces/IOrderRepository.cs
@@ -1,13 +1,13 @@
 ﻿using Contracts.Domains.Interfaces;
 using Ordering.Domain.Entities;
 
-namespace Ordering.Application.Common.Interfaces
+namespace Ordering.Application.Common.Interfaces;
+
+public interface IOrderRepository : IRepositoryBaseAsync<Order, long>
 {
-    public interface IOrderRepository : IRepositoryBaseAsync<Order, long>
-    {
-        Task<IEnumerable<Order>> GetOrdersByUserNameAsync(string userName);
-        void CreateOrder(Order order);
-        Task<Order> UpdateOrderAsync(Order order);
-        void DeleteOrder(Order order);
-    }
+    Task<IEnumerable<Order>> GetOrdersByUserNameAsync(string userName);
+    Task<Order> GetOrderByDocumentNo(string documentNo);
+    void CreateOrder(Order order);
+    Task<Order> UpdateOrderAsync(Order order);
+    void DeleteOrder(Order order);
 }
